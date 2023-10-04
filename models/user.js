21924112b4d2d699963e4e2b6ac89782ccc47e53
hui-kelvin <kelvinhui852@gmail.com>
2023-10-04 12:45:16 -0700

    updated models

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcrypt');
 
 class User{
-    constructor(username,password) {
+    constructor(username,password,gender,weight,height) {
         this.username = username;
         this.password = password;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
         this.role = "user";
     }
 }
