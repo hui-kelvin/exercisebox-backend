b6c23ef756982555ab880da365bd24d4e6a528a3
Michael <1992mmoore22@gmail.com>
2023-10-05 15:01:52 -0700

    Implemented exercise search feature

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 9000;
+const bodyParser = require('body-parser');
+const exercisesRoute = require('./routes/exerciseRoutes');
 
-
-
+app.use(bodyParser.json());
+app.use('/exercises', exercisesRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ services/exerciseService.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+const KEY = fs.readFileSync('rapidAPIkey.txt', { encoding: 'utf8', flag: 'r' });
+
+async function getExercises(params) {
+    const queryString = buildQueryString(params);
+
+    const URL = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?${queryString}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': KEY,
+            'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(URL, options);
+        const exercises = await response.json();
+        return exercises;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/*----------------- HELPER FUNCTIONS -----------------*/
+
+// Constructs the query string to pass into a URL
+function buildQueryString(params) {
+    const keys = Object.keys(params);
+    const values = Object.values(params);
+    let queryString = '';
+
+    for (i = 0; i < keys.length; i++) {
+        if (i == keys.length - 1)
+            queryString += `${keys[i]}=${values[i]}`;
+
+        else
+            queryString += `${keys[i]}=${values[i]}&`;
+    }
+
+    return queryString;
+}
+
+/*--------------- END HELPER FUNCTIONS ---------------*/
+
+module.exports = { getExercises };
